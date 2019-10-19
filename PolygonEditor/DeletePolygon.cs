f9107cc6811c3c66f2f197acb48c426c9a3aa4cf
Ireneusz Stanicki <ireneusz.stanicki@itboom.pl>
2019-10-19 11:15:20 +0200

    Get distance from line bug fixed (replace by get distance from segment)

--- PolygonEditor/DeletePolygon.cs
+++ PolygonEditor/DeletePolygon.cs
@@ -19,7 +19,7 @@ namespace PolygonEditor
             {
                 foreach(var segment in polygon.segments)
                 {
-                    if (GetDistanceFromLine(segment, p) <= 3)
+                    if (GetDistanceFromLine(segment, p) <= 3 && IsPointBetween(p,segment))
                         return (polygon,segment);
                 }
             }
@@ -41,6 +41,25 @@ namespace PolygonEditor
         {
             return Math.Abs((segment.b.X - segment.a.X) * (segment.a.Y - p.Y) - (segment.a.X - p.X) * (segment.b.Y - segment.a.Y)) /
                     Math.Sqrt(Math.Pow(segment.b.X - segment.a.X, 2) + Math.Pow(segment.b.Y - segment.a.Y, 2));
+        }
+
+        private bool IsPointBetween(Point p, (Point p1, Point p2) segment)
+        {
+            int minX = segment.p2.X, maxX = segment.p1.X ,minY = segment.p2.Y, maxY = segment.p1.Y;
+
+            if(segment.p2.X > segment.p1.X)
+            {
+                minX = segment.p1.X;
+                maxX = segment.p2.X;
+            }
+
+            if(segment.p2.Y > segment.p1.Y)
+            {
+                minY = segment.p1.Y;
+                maxY = segment.p2.Y;
+            }
+
+            return p.X >= minX && p.X <= maxX && p.Y >= minY && p.Y <= maxY;
         }
     }
 }
